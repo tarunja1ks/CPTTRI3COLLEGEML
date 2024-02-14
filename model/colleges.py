2028ba4bf84ba5a5e6b8f87fa1d5de195955193e
sfremy <myre.wx@gmail.com>
2024-02-13 20:06:02 -0800

    properties pt 1

--- model/colleges.py
+++ model/colleges.py
@@ -24,17 +24,16 @@ class College(db.Model):
     __tablename__ = 'colleges'  # table name is plural, class name is singular
 
     # Define the Player schema with "vars" from object
-    id = db.Column(db.Integer, primary_key=True)
     _name = db.Column(db.String(255), unique=False, nullable=False)
-    _uid = db.Column(db.String(255), unique=True, nullable=False)
-    _password = db.Column(db.String(255), unique=False, nullable=False)
+    _link = db.Column(db.String(255), unique=True, nullable=False)
+    _type = db.Column(db.String(255), unique=False, nullable=False)
     _tokens = db.Column(db.Integer)    
 
     # constructor of a Player object, initializes the instance variables within object (self)
-    def __init__(self, name, uid, tokens, password="123qwerty"):
+    def __init__(self, name, link, type, tokens):
         self._name = name    # variables with self prefix become part of the object, 
-        self._uid = uid
-        self.set_password(password)
+        self._link = link
+        self.type = type
         self._tokens = tokens
 
     # a name getter method, extracts name from object
@@ -51,41 +50,6 @@ class College(db.Model):
     @property
     def uid(self):
         return self._uid
-    
-    # a setter function, allows name to be updated after initial object creation
-    @uid.setter
-    def uid(self, uid):
-        self._uid = uid
-        
-    # check if uid parameter matches user id in object, return boolean
-    def is_uid(self, uid):
-        return self._uid == uid
-    
-    @property
-    def password(self):
-        return self._password[0:10] + "..." # because of security only show 1st characters
-
-    # update password, this is conventional setter
-    def set_password(self, password):
-        """Create a hashed password."""
-        self._password = generate_password_hash(password, "pbkdf2:sha256", salt_length=10)
-        
-    # check password parameter versus stored/encrypted password
-    def is_password(self, password):
-        """Check against hashed password."""
-        result = check_password_hash(self._password, password)
-        return result
-    
-    # dob property is returned as string, to avoid unfriendly outcomes
-    @property
-    def tokens(self):
-        return self._tokens
-    
-    # dob should be have verification for type date
-    @tokens.setter
-    def tokens(self, tokens):
-        self._tokens = tokens
-    
     
     # output content using str(object) in human readable form, uses getter
     # output content using json dumps, this is ready for API response
