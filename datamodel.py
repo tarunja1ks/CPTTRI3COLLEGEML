2991c042b4331188369be6abc3282ae13956d0ff
Ankit-177 <ankit.pulivendula@gmail.com>
2024-03-28 15:04:59 -0700

    Fixes for efficiency

--- datamodel.py
+++ datamodel.py
@@ -5,20 +5,21 @@ class datamodel:
     
     def __init__(self): # Loading in the model file (this includes all weights after training) / needs to be re-run every time
         self.model = joblib.load("College_prediction.pkl")
-    def readfile(self):
+    def readfile(self): # one and done since it just obtains training data once and for all
         self.data=pd.read_csv("student_admission_dataset.csv")
         print(self.data.head())
-    def preprocessing(self):# this is loading in the file stuff into X and Y
+    def preprocessing(self):# this is loading in the file stuff into X and Y / one and done because we're just defining variables
         self.X=self.data[['GPA','SAT_Score','Extracurricular_Activities']]
         self.Y=self.data['Admission_Status']
-    def train(self): # this is actually training the model
+    def train(self): # this is actually training the model / one and done; no need to re-train every time
         self.model=LogisticRegression()
         self.model.fit(self.X,self.Y)
-    def predict(self, GPA, SAT_SCORE, EXTRACIRICULAR): # this is prediciton function
-        return self.model.predict([[GPA, SAT_SCORE, EXTRACIRICULAR]])[0]
-    
+        
     def exportmodel(self): # after training the model, we have to export to permanently save weights to .pkl file / one and done
         joblib.dump(self.model,"College_prediction.pkl")
+        
+    def predict(self, GPA, SAT_SCORE, EXTRACIRICULAR): # this is prediciton function
+        return self.model.predict([[GPA, SAT_SCORE, EXTRACIRICULAR]])[0]
     
 
     ## this stuff is just crud 
